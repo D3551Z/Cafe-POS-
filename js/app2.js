ecd94359cdc3da8f842821f651d8a366875521ba
JoakimNylund <50205087+JoakimNylund@users.noreply.github.com>
2019-10-21 19:27:10 +0800

    Add files via upload

--- js/app2.js
+++ js/app2.js
@@ -15,23 +15,132 @@
 
    
 
-    const txtEmail = document.getElementById('txtEmail');
-    const txtPassword = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
-    const btnSignUp = document.getElementById('btnSignUp');
-    const btnLogout = document.getElementById('btnLogout');
-    const auth = firebase.auth();
-    const db = firebase.firestore();
-    const form = document.querySelector('#survey');
+        const txtEmail = document.getElementById('txtEmail');
+        const txtPassword = document.getElementById('txtPassword');
+        const btnLogin = document.getElementById('btnLogin');
+        const btnSignUp = document.getElementById('btnSignUp');
+        const btnLogout = document.getElementById('btnLogout');
+        const button1 = document.getElementById('button1');
+        const button2 = document.getElementById('button2');
+        const button3 = document.getElementById('button3');
 
-form.addEventListener('submit', (e) => {
-    console.log("heeeej");
+
+        const auth = firebase.auth();
+        const db = firebase.firestore();
+        const form = document.querySelectorAll('.survey');
+        const tableID = document.getElementsByClassName("tableIDe");
+        const rank1 = document.getElementById('rank1');
+        const cus1 = document.getElementById('cus1');
+const cus2 = document.getElementById('cus2');
+const cus3 = document.getElementById('cus3');
+
+
+
+
+
+/*var docRef = db.collection('Table').doc(tableID);
+
+docRef.get().then(function (doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function (error) {
+    console.log("Error getting document:", error);
+}); */
+
+button1.addEventListener('click', (e) => {
     e.preventDefault();
-    db.collection('Table').add({
-        status: form.rank.value,
+    console.log("hej");
+
+    db.collection('Table').doc("Table 1").update({
+
+        status: rank1.value,
+        customer_info: cus1.value
+
     });
 });
 
+button2.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("tabell2");
+
+    db.collection('Table').doc("Table 2").update({
+
+        status: rank2.value,
+        customer_info: cus2.value
+
+    });
+});
+
+button3.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("tabell3");
+
+    db.collection('Table').doc("Table 3").update({
+
+        status: rank3.value,
+        customer_info: cus3.value
+
+    });
+});
+
+/* for (var i = 0; i < form.length; i++) {
+    form[i].addEventListener('submit', (e) => {
+
+        e.preventDefault();
+
+        for (var i = 0; i < tableID.length; i++) {
+            var tableNumber = tableID[i].textContent
+            console.log(tableID[i].textContent);
+
+            var docRef = db.collection("Table").doc('Table 2');
+            console.log(docRef + " yoooo");
+            docRef.get().then(function (doc) {
+                console.log("inside function" + docRef.get());
+                if (doc = "Table 1") {
+                    console.log(doc + "+ woow");
+                    console.log(tableNumber + " is the table ID");
+                    db.collection('Table').doc("Table 1").update({
+                       // status: form.rank1.value,
+                        status: rank1.value,
+                       // customer_info: form.customer_info.value
+                        customer_info: cust_info.value
+
+                    });
+             
+                    console.log("No such document!");
+                }
+            })
+
+        }
+
+
+    });
+
+
+} */
+
+
+
+
+    //     db.collection('Table').where('id', '==', "Table 1").get(); {
+
+
+
+
+    //Change select-color
+    /*   function onSelectChange() {
+           if (document.getElementsByClassName('rank').value == unoccupied) {
+             document.getElementsByClassName('rank').className = 'myRed';
+           } else {
+               document.getElementById('mySelect').className = 'myBlack'
+           }
+       
+       }
+   }); */
 
     //Login
     btnLogin.addEventListener('click', e => {
@@ -60,28 +169,28 @@ form.addEventListener('submit', (e) => {
         console.log(" user logged out");
     });
 
-$(document).ready(function () {
-    $("#buttoner").click(function () {
-        $("#box form").toggle("slow");
-        if (user) {
-            // User is signed in.
-            console.log("signed in");
-        } else {
-            // No user is signed in.
-        }
-        return false;
-    });
-});
-
-$(document).ready(function () {
-
-    $("#buttoner1").click(function () {
-        $("#box ").toggle("slow");
-        return false;
+    $(document).ready(function () {
+        $("#buttoner").click(function () {
+            $("#box form").toggle("slow");
+            if (user) {
+                // User is signed in.
+                console.log("signed in");
+            } else {
+                // No user is signed in.
+            }
+            return false;
+        });
     });
 
+    $(document).ready(function () {
 
-});
+        $("#buttoner1").click(function () {
+            $("#box ").toggle("slow");
+            return false;
+        });
+
+
+    });
 
     // Checking if the user is logged in or not.
     function checkUser() {
@@ -97,19 +206,19 @@ $(document).ready(function () {
 
     checkUser();
 
-// Table management.
-function CloneForm(formName) {
-    var tableCount = 2;
-    document.getElementById('tableID').innerHTML++;
-    tableCount++;
+    // Table management.
+    function CloneForm(formName) {
+        var tableCount = 2;
+        document.getElementsByClassName('tableIDe').innerHTML++;
+        tableCount++;
 
 
-    var formCount = document.forms.length;
-    var oForm = document.forms[formName];
-    var clone = oForm.cloneNode(true);
-    clone.name += "_" + formCount;
-    document.body.appendChild(clone);
+        var formCount = document.forms.length;
+        var oForm = document.forms[formName];
+        var clone = oForm.cloneNode(true);
+        clone.name += "_" + formCount;
+        document.body.appendChild(clone);
 
-}
+    }
 
 
